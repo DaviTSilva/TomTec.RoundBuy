d0868aedd051305150d4a3275bf974a99d2756ea
DaviTSilva <davithomazellaes@gmail.com>
2022-11-19 11:16:50 -0300

    Add project files.

---
+++ TomTec.RoundBuy.API/Global.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TomTec.RoundBuy.API
+{
+    public static class Global
+    {
+        private static IConfiguration _configuration;
+
+        public static IConfiguration Configuration
+        {
+            get
+            {
+                return _configuration;
+            }
+            set
+            {
+                _configuration = value;
+            }
+        }
+    }
+}
