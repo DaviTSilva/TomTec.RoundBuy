8e08d5c4820f8427ad7807a8e8379b90bb7f5a25
DaviTSilva <davithomazellaes@gmail.com>
2023-04-04 19:56:55 -0300

    payment method controller

--- TomTec.RoundBuy.API/Controllers/v1/Sales/PaymentMethodController.cs
+++ TomTec.RoundBuy.API/Controllers/v1/Sales/PaymentMethodController.cs
@@ -1,11 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TomTec.RoundBuy.API.DTOs.v1;
+using TomTec.RoundBuy.Data;
+using TomTec.RoundBuy.Lib.AspNetCore;
 using TomTec.RoundBuy.Lib.AspNetCore.Filters;
+using TomTec.RoundBuy.Models;
 
-namespace TomTec.RoundBuy.API.Controllers.v1.Sales
+namespace TomTec.RoundBuy.API.Controllers.v1
 {
     [Route("v1/payment-methods")]
     [ServiceFilter(typeof(KeyNotFoundExceptionFilterAttribute))]
@@ -13,9 +18,64 @@ namespace TomTec.RoundBuy.API.Controllers.v1.Sales
     [ServiceFilter(typeof(GenericExceptionFilterAttribute))]
     public class PaymentMethodController : Controller
     {
-        public IActionResult CratePaymmentMethod()
+        private readonly IRepository<PaymentMethod> _paymentMethodRepository;
+
+        public PaymentMethodController(IRepository<PaymentMethod> paymentMethodRepository)
         {
-            return View();
+            _paymentMethodRepository = paymentMethodRepository;
+        }
+
+        [HttpPost("")]
+        [Authorize(Roles = "manager")]
+        public IActionResult CratePaymmentMethod([FromBody] PaymentMethodDto paymentModelDto)
+        {
+            var paymentMethod = _paymentMethodRepository.Create(paymentModelDto.ToModel());
+            return Created(ResponseMessage.Success, paymentMethod);
+        }
+
+        [HttpGet("")]
+        public IActionResult GetPaymentMethods()
+        {
+            var paymentMethods = _paymentMethodRepository.Get();
+            return Ok(new {
+                message = ResponseMessage.Success,
+                value = paymentMethods,
+            });
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetPaymentMethodById(int id)
+        {
+            var paymentMethod = _paymentMethodRepository.Get(id);
+            return Ok(new { 
+                message = ResponseMessage.Success,
+                value = paymentMethod,
+            });
+        }
+
+        [HttpPut("{id}")]
+        [Authorize(Roles = "manager")]
+        public IActionResult UpdatePaymentMethod(int id, [FromBody]PaymentMethodDto paymentMethodDto)
+        {
+            var paymentMethod = paymentMethodDto.ToModel();
+            paymentMethod.Id = id;
+            _paymentMethodRepository.Update(paymentMethod);
+
+            return Ok(new
+            {
+                message = ResponseMessage.Success,
+            });
+        }
+
+        [HttpDelete("{id}")]
+        [Authorize(Roles = "manager")]
+        public IActionResult DeletePaymentMethod(int id)
+        {
+            _paymentMethodRepository.Delete(id);
+            return Ok(new
+            {
+                message = ResponseMessage.Success,
+            });
         }
     }
 }
