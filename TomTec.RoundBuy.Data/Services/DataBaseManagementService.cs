76c652c329f17e05fbd8f107358085fb16c776f6
DaviTSilva <davithomazellaes@gmail.com>
2023-04-24 16:43:45 -0300

    adding docker compability

---
+++ TomTec.RoundBuy.Data/Services/DataBaseManagementService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TomTec.RoundBuy.Data
+{
+    public static class DataBaseManagementService
+    {
+        public static void MigrationInitialization(this IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                var serviceDB = serviceScope.ServiceProvider.GetService<RoundBuyDbContext>();
+                serviceDB.Database.Migrate();
+            }
+        }
+    }
+}
