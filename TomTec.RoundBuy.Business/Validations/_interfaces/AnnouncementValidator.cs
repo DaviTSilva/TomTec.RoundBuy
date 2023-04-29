b3ea892d765cea12e209979d7c3849786857d439
DaviTSilva <davithomazellaes@gmail.com>
2023-04-29 05:53:48 -0300

    Profanity Detector

---
+++ TomTec.RoundBuy.Business/Validations/_interfaces/AnnouncementValidator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TomTec.RoundBuy.Models;
+using TomTec.RoundBuy.Lib.Utils;
+using System.Linq;
+
+namespace TomTec.RoundBuy.Business
+{
+    static public class AnnouncementValidator
+    {
+        static public void Validate(this Announcement announcement)
+        {
+            if (string.IsNullOrEmpty(announcement.Title))
+                throw new InvalidOperationException("Announcement title can not be empty!");
+
+            if (announcement.Title.ContainsProfanity()
+                || announcement.Description.ContainsProfanity()
+                || announcement.ProductPacks.Any(x => x.Product.Title.ContainsProfanity() 
+                    || x.Product.Model.ContainsProfanity() 
+                    || x.Product.TechnicalInfos.Any(y => y.Title.ContainsProfanity() 
+                        || y.InfoSeparetedBySemicolon.ContainsProfanity()
+                        )
+                    )
+                )
+                throw new InvalidOperationException("Profane words are not allowed!");
+
+        }
+    }
+}
