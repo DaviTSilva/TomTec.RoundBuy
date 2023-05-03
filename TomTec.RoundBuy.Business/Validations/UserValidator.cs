64ed94289c8c1538ddd08ee6097ce40afc93ae72
DaviTSilva <davithomazellaes@gmail.com>
2023-05-03 03:55:32 -0300

    UserValidation

---
+++ TomTec.RoundBuy.Business/Validations/UserValidator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TomTec.RoundBuy.Models;
+
+namespace TomTec.RoundBuy.Business.Validations
+{
+    static public class UserValidator
+    {
+        static public void Validate(this User user)
+        {
+
+            //Profanity Detector
+            if (string.IsNullOrEmpty(user.UserName))
+                throw new InvalidOperationException($"[INVALID \"{nameof(User).ToUpper()}\"! WRONG VALUE FOR FIELD \"{nameof(User.UserName).ToUpper()}\"]: Username can not contain profane words!");
+        }
+
+
+    }
+}
